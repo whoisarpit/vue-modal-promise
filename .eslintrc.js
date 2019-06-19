44ade1754d9d4979c6d448278300f4937c53ae36
Arpit Roopchandani <arpit9295@gmail.com>
2019-06-19 07:42:21 +0100

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,8 +9,40 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'import/extensions': ['error', 'always', {
+      js: 'never',
+    }],
+    // disallow reassignment of function parameters
+    // disallow parameter object manipulation except for specific exclusions
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'state', // for vuex state
+        'acc', // for reduce accumulators
+        'e', // for e.returnvalue
+      ],
+    }],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': ['error', {
+      optionalDependencies: ['test/unit/index.js'],
+    }],
+    // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+    // * Custom rules after this line
+
+    'vue/script-indent': ['error', 2, {
+      baseIndent: 1,
+    }],
   },
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        indent: 'off',
+      },
+    },
+  ],
   parserOptions: {
     parser: 'babel-eslint',
   },
